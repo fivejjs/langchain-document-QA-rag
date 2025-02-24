ca24ae874a05f8986d1652994ddb5a92f8633737
jinjun <jsunster@gmail.com>
2025-02-24 16:29:58 +1100

    update the sytle

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const standaloneQuestionPrompt = PromptTemplate.fromTemplate(standaloneQuestionT
 
 const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided. 
 Try to find the answer in the context. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." 
-And direct the questioner to email help@scrimba.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.
+And direct the questioner to email <change_to@your.email.com>. Don't try to make up an answer. Always speak as if you were chatting to a friend.
 context: {context}
 question: {question}
 answer: `
